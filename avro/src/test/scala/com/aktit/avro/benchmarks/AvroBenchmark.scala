01460314cc27a38fd924cce328cd16026b63fb32
kostas.kougios@gmail.com <kostas.kougios@googlemail.com>
2018-05-21 15:47:15 +0100

    avro benchmarks

--- avro/src/test/scala/com/aktit/avro/benchmarks/AvroBenchmark.scala
+++ avro/src/test/scala/com/aktit/avro/benchmarks/AvroBenchmark.scala
@@ -1,6 +1,7 @@
 package com.aktit.avro.benchmarks
 
-import com.aktit.avro.serializers.DTOSerializers
+import com.aktit.avro.serializers.DTOSerializers.pageSerializer
+import com.aktit.wikipedia.dto.Page
 import com.aktit.wikipedia.dto.WikipediaBuilder.page
 import org.apache.commons.lang3.SerializationUtils
 
@@ -14,7 +15,13 @@ object AvroBenchmark extends App
 	val pg = page()
 
 	measure("java serialization", SerializationUtils.serialize(pg))
-	measure("avro serialization", DTOSerializers.pageSerializer.serializeSingleBinary(pg))
+	val jData = SerializationUtils.serialize(pg)
+	measure("java deserialization", SerializationUtils.deserialize[Page](jData))
+
+	measure("avro serialization", pageSerializer.serializeSingleBinary(pg))
+
+	val aData = pageSerializer.serializeSingleBinary(pg)
+	measure("avro deserialization", pageSerializer.deserializeSingleBinary(aData))
 
 	def measure(title: String, f: => AnyRef) = {
 
