394368f4c799c83b2a830f22c21f192b0c936532
kostas.kougios@gmail.com <kostas.kougios@googlemail.com>
2018-05-25 22:55:09 +0100

    prototyping GOL domain model

--- gameOfLife/src/test/scala/com/aktit/gameoflife/model/BoundariesTest.scala
+++ gameOfLife/src/test/scala/com/aktit/gameoflife/model/BoundariesTest.scala
@@ -11,12 +11,34 @@ import org.scalatest.Matchers._
 class BoundariesTest extends FunSuite
 {
 	test("isTop") {
-		val b = boundaries(top = Array(-1, 3, 7, 11))
-		b.isTop(-1) should be(true)
-		b.isTop(0) should be(false)
-		b.isTop(3) should be(true)
-		b.isTop(4) should be(false)
-		b.isTop(10) should be(false)
-		b.isTop(11) should be(true)
+		val live = Array(-1, 3, 7, 11)
+		val b = boundaries(top = live)
+		for (x <- -1 to 11) {
+			b.isTop(x) should be(live.contains(x))
+		}
+	}
+
+	test("isBottom") {
+		val live = Array(-1, 3, 7, 11)
+		val b = boundaries(bottom = live)
+		for (x <- -1 to 11) {
+			b.isBottom(x) should be(live.contains(x))
+		}
+	}
+
+	test("isLeft") {
+		val live = Array(0, 2, 3, 5)
+		val b = boundaries(left = live)
+		for (y <- 0 to 5) {
+			b.isLeftLive(y) should be(live.contains(y))
+		}
+	}
+
+	test("isRight") {
+		val live = Array(0, 2, 3, 5)
+		val b = boundaries(right = live)
+		for (y <- 0 to 5) {
+			b.isRightLive(y) should be(live.contains(y))
+		}
 	}
 }
