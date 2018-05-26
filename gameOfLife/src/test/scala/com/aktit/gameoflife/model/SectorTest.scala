429321b74fe46282933fc47602c58a99a79ba75c
kostas.kougios@gmail.com <kostas.kougios@googlemail.com>
2018-05-26 19:44:42 +0100

    prototyping GOL domain model

--- gameOfLife/src/test/scala/com/aktit/gameoflife/model/SectorTest.scala
+++ gameOfLife/src/test/scala/com/aktit/gameoflife/model/SectorTest.scala
@@ -33,4 +33,21 @@ class SectorTest extends FunSuite
 	test("isLive right boundary positive") {
 		sector(boundaries = boundaries(right = Array(2))).isLive(10, 2) should be(true)
 	}
+
+	test("isLive top boundary negative") {
+		sector().isLive(5, -1) should be(false)
+	}
+
+	test("isLive top boundary positive") {
+		sector(boundaries = boundaries(top = Array(5))).isLive(5, -1) should be(true)
+	}
+
+	test("isLive bottom boundary negative") {
+		sector().isLive(3, 5) should be(false)
+	}
+
+	test("isLive bottom boundary positive") {
+		sector(boundaries = boundaries(bottom = Array(3))).isLive(3, 5) should be(true)
+	}
+
 }
