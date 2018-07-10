af7b72e321a8d9e5c501895a3c9042a1705cf8ea
kostas.kougios@gmail.com <kostas.kougios@googlemail.com>
2018-07-10 15:27:55 +0100

    benchmarking file formats

--- sql/src/main/scala/com/aktit/sql/performance/BenchmarkImpressions.scala
+++ sql/src/main/scala/com/aktit/sql/performance/BenchmarkImpressions.scala
@@ -1,10 +1,11 @@
 package com.aktit.sql.performance
 
+import java.time.Instant
+
 import com.aktit.utils.TimeMeasure
 import com.databricks.spark.avro._
 import org.apache.spark.internal.Logging
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.types._
 
 /**
   * @author kostas.kougios
@@ -20,6 +21,12 @@ object BenchmarkImpressions extends Logging
 		withCorrectSchema(spark).avro("/tmp/impressions/avro").createOrReplaceTempView("impressions_avro")
 		withCorrectSchema(spark).parquet("/tmp/impressions/parquet").createOrReplaceTempView("impressions_parquet")
 
+		val fromInstance = Instant.parse("2010-01-01T00:00:00.00Z").toEpochMilli
+		val toInstance = Instant.parse("2010-02-01T00:00:00.00Z").toEpochMilli
+
+		val (avroDistinctForDateUsers, _) = TimeMeasure.dt(spark.sql(s"select count(distinct userId) from impressions_avro where date between $fromInstance and $toInstance").show())
+		val (parquetDistinctForDateUsers, _) = TimeMeasure.dt(spark.sql("select count(distinct userId) from impressions_parquet where date between '2010-01-01T00:00:00.00Z' and '2010-02-01T00:00:00.00Z'").show())
+
 		val (avroImpressions, _) = TimeMeasure.dt(spark.sql("select count(*) from impressions_avro").show())
 		val (parquetImpressions, _) = TimeMeasure.dt(spark.sql("select count(*) from impressions_parquet").show())
 
@@ -34,14 +41,17 @@ object BenchmarkImpressions extends Logging
 			   |
 			   |select count(*) : $avroImpressions / $parquetImpressions
 			   |select count(distinct userId) : $avroDistinctUsers / $parquetDistinctUsers
+			   |select count(distinct userId) from impressions_avro where date between : $avroDistinctForDateUsers/$parquetDistinctForDateUsers
 			""".stripMargin)
 	}
 
-	private def withCorrectSchema(spark: SparkSession) = spark.read.schema(
-		StructType(Seq(
-			StructField("userId", LongType, false),
-			StructField("date", TimestampType, false),
-			StructField("refererUrl", StringType, true)
-		))
-	)
+	private def withCorrectSchema(spark: SparkSession) = spark.read
+
+	//		.schema(
+	//		StructType(Seq(
+	//			StructField("userId", LongType, false),
+	//			StructField("date", TimestampType, false),
+	//			StructField("refererUrl", StringType, true)
+	//		))
+	//	)
 }
