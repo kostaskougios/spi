1edecb7b36de4545325b3065ea51b3acb5bfdc51
kostas.kougios@gmail.com <kostas.kougios@googlemail.com>
2018-07-11 11:16:35 +0100

    benchmarking file formats

--- sql/src/main/scala/com/aktit/sql/performance/BenchmarkImpressions.scala
+++ sql/src/main/scala/com/aktit/sql/performance/BenchmarkImpressions.scala
@@ -18,27 +18,35 @@ object BenchmarkImpressions extends Logging
 
 		val spark = SparkSession.builder.config("spark.sql.orc.impl", "native").getOrCreate
 
-		withCorrectSchema(spark).avro("/tmp/impressions/avro").createOrReplaceTempView("impressions_avro")
-		withCorrectSchema(spark).parquet("/tmp/impressions/parquet").createOrReplaceTempView("impressions_parquet")
-		withCorrectSchema(spark).orc("/tmp/impressions/orc").createOrReplaceTempView("impressions_orc")
+		def measureQuery(q: String => String) = {
+			val (avro, _) = TimeMeasure.dt(spark.sql(q("avro")).show())
+			val (parquet, _) = TimeMeasure.dt(spark.sql(q("parquet")).show())
+			val (orc, _) = TimeMeasure.dt(spark.sql(q("orc")).show())
+			(avro, parquet, orc)
+		}
 
-		val StartDate = "2010-02-01T00:00:00.00Z"
-		// avro converts timestamps to Long and we have to query it based on long values
-		val fromInstance = Instant.parse(StartDate).toEpochMilli
-		val EndDate = "2010-03-01T00:00:00.00Z"
-		val toInstance = Instant.parse(EndDate).toEpochMilli
+		def measureDateQuery(startDate: String, endDate: String, q: (String, String, String) => String) = {
+			// avro converts timestamps to Long and we have to query it based on long values
+			val fromInstance = Instant.parse(startDate).toEpochMilli
+			val toInstance = Instant.parse(endDate).toEpochMilli
 
-		val (avroDistinctForDateUsers, _) = TimeMeasure.dt(spark.sql(s"select count(distinct userId) from impressions_avro where date between $fromInstance and $toInstance").show())
-		val (parquetDistinctForDateUsers, _) = TimeMeasure.dt(spark.sql(s"select count(distinct userId) from impressions_parquet where date between '$StartDate' and '$EndDate'").show())
-		val (orcDistinctForDateUsers, _) = TimeMeasure.dt(spark.sql(s"select count(distinct userId) from impressions_orc where date between '$StartDate' and '$EndDate'").show())
+			val (avro, _) = TimeMeasure.dt(spark.sql(q("avro", fromInstance.toString, toInstance.toString)).show())
+			val (parquet, _) = TimeMeasure.dt(spark.sql(q("parquet", startDate, endDate)).show())
+			val (orc, _) = TimeMeasure.dt(spark.sql(q("orc", startDate, endDate)).show())
+			(avro, parquet, orc)
 
-		val (avroImpressions, _) = TimeMeasure.dt(spark.sql("select count(*) from impressions_avro").show())
-		val (parquetImpressions, _) = TimeMeasure.dt(spark.sql("select count(*) from impressions_parquet").show())
-		val (orcImpressions, _) = TimeMeasure.dt(spark.sql("select count(*) from impressions_orc").show())
+		}
 
-		val (avroDistinctUsers, _) = TimeMeasure.dt(spark.sql("select count(distinct userId) from impressions_avro").show())
-		val (parquetDistinctUsers, _) = TimeMeasure.dt(spark.sql("select count(distinct userId) from impressions_parquet").show())
-		val (orcDistinctUsers, _) = TimeMeasure.dt(spark.sql("select count(distinct userId) from impressions_orc").show())
+		spark.read.avro("/tmp/impressions/avro").createOrReplaceTempView("impressions_avro")
+		spark.read.parquet("/tmp/impressions/parquet").createOrReplaceTempView("impressions_parquet")
+		spark.read.orc("/tmp/impressions/orc").createOrReplaceTempView("impressions_orc")
+
+		val (avroDistinctForDateUsers, parquetDistinctForDateUsers, orcDistinctForDateUsers) =
+			measureDateQuery("2010-02-01T00:00:00.00Z", "2010-03-01T00:00:00.00Z", (t, start, end) => s"select count(distinct userId) from impressions_$t where date between '$start' and '$end'")
+
+		val (avroImpressions, parquetImpressions, orcImpressions) = measureQuery(t => s"select count(*) from impressions_$t")
+		val (avroDistinctUsers, parquetDistinctUsers, orcDistinctUsers) = measureQuery(t => s"select count(distinct userId) from impressions_$t")
+
 
 		logInfo(
 			s"""
@@ -50,15 +58,7 @@ object BenchmarkImpressions extends Logging
 			   |select count(distinct userId) : $avroDistinctUsers / $parquetDistinctUsers / $orcDistinctUsers
 			   |select count(distinct userId) from impressions_avro where date between : $avroDistinctForDateUsers/$parquetDistinctForDateUsers/$orcDistinctForDateUsers
 			""".stripMargin)
+
 	}
 
-	private def withCorrectSchema(spark: SparkSession) = spark.read
-
-	//		.schema(
-	//		StructType(Seq(
-	//			StructField("userId", LongType, false),
-	//			StructField("date", TimestampType, false),
-	//			StructField("refererUrl", StringType, true)
-	//		))
-	//	)
 }
