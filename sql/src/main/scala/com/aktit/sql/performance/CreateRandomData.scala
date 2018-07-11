0694a8550d4fc781c5fb8d76a15017d3ca28604d
kostas.kougios@gmail.com <kostas.kougios@googlemail.com>
2018-07-11 14:47:44 +0100

    orders

--- sql/src/main/scala/com/aktit/sql/performance/CreateRandomData.scala
+++ sql/src/main/scala/com/aktit/sql/performance/CreateRandomData.scala
@@ -2,6 +2,7 @@ package com.aktit.sql.performance
 
 import java.sql.Timestamp
 import java.time.Instant
+import java.util.UUID
 
 import org.apache.spark.internal.Logging
 import org.apache.spark.sql.SparkSession
@@ -11,18 +12,13 @@ import scala.util.Random
 /**
   * Create random impression data to be used by the benchmarks.
   *
-  * Takes 1h 15m on i7 4770k with SSD drive.
-  *
-  * 11G	/tmp/impressions/avro
-  * 16G	/tmp/impressions/parquet
-  * 27G	/tmp/impressions
-  *
   * @author kostas.kougios
   *         10/07/18 - 09:43
   */
-object CreateImpressions extends Logging
+object CreateRandomData extends Logging
 {
 	val MaxUsers = 1000000
+	val MaxProducts = 5000
 	val Group = 5000000 // reduce this if you have less memory
 
 	def main(args: Array[String]): Unit = {
@@ -35,12 +31,37 @@ object CreateImpressions extends Logging
 		logInfo(s"Impressions: Will append ${impressions / Group} times")
 
 		val startClock = Instant.parse("2010-01-01T00:00:00.00Z")
-		val testData = for (i <- (1l to impressions).toIterator)
-			yield PageImpression(Random.nextInt(MaxUsers), Timestamp.from(startClock.plusSeconds(i)), s"http://www.some-server.com/part1/part2/$i")
 
 		new Creator(
 			spark,
-			testData,
+			for (i <- (1l to impressions).toIterator) yield {
+				val productId = Random.nextInt(MaxProducts)
+				val price = productId % 250
+				val discount = Random.nextInt(30)
+				Order(
+					Random.nextInt(MaxUsers),
+					UUID.randomUUID.toString,
+					Timestamp.from(startClock.plusSeconds(productId)),
+					productId,
+					s"product-code-for-$productId",
+					s"product-title-for-$productId",
+					price,
+					price * (1.00f - discount.toFloat / 100),
+					discount.toByte
+				)
+			},
+			impressionsTargetDir + "/orders",
+			Group
+		).create()
+
+		new Creator(
+			spark,
+			for (i <- (1l to impressions).toIterator) yield
+				PageImpression(
+					Random.nextInt(MaxUsers),
+					Timestamp.from(startClock.plusSeconds(i)),
+					s"http://www.some-server.com/part1/part2/$i"
+				),
 			impressionsTargetDir + "/impressions",
 			Group
 		).create()
