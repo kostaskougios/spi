61cb00336caa0081787dfa086a5803136e259e46
kostas.kougios@gmail.com <kostas.kougios@googlemail.com>
2018-07-11 11:33:33 +0100

    benchmarking file formats

--- sql/src/main/scala/com/aktit/sql/performance/BenchmarkImpressions.scala
+++ sql/src/main/scala/com/aktit/sql/performance/BenchmarkImpressions.scala
@@ -8,6 +8,8 @@ import org.apache.spark.internal.Logging
 import org.apache.spark.sql.SparkSession
 
 /**
+  * Benchmark the Impressions table, a table with a small number of columns
+  *
   * @author kostas.kougios
   *         10/07/18 - 09:43
   */
@@ -22,7 +24,7 @@ object BenchmarkImpressions extends Logging
 			val (avro, _) = TimeMeasure.dt(spark.sql(q("avro")).show())
 			val (parquet, _) = TimeMeasure.dt(spark.sql(q("parquet")).show())
 			val (orc, _) = TimeMeasure.dt(spark.sql(q("orc")).show())
-			(avro, parquet, orc)
+			Seq(q("*"), avro, parquet, orc)
 		}
 
 		def measureDateQuery(startDate: String, endDate: String, q: (String, String, String) => String) = {
@@ -33,7 +35,7 @@ object BenchmarkImpressions extends Logging
 			val (avro, _) = TimeMeasure.dt(spark.sql(q("avro", fromInstance.toString, toInstance.toString)).show())
 			val (parquet, _) = TimeMeasure.dt(spark.sql(q("parquet", startDate, endDate)).show())
 			val (orc, _) = TimeMeasure.dt(spark.sql(q("orc", startDate, endDate)).show())
-			(avro, parquet, orc)
+			Seq(q("*", startDate, endDate), avro, parquet, orc)
 
 		}
 
@@ -41,21 +43,14 @@ object BenchmarkImpressions extends Logging
 		spark.read.parquet("/tmp/impressions/parquet").createOrReplaceTempView("impressions_parquet")
 		spark.read.orc("/tmp/impressions/orc").createOrReplaceTempView("impressions_orc")
 
-		val (avroDistinctForDateUsers, parquetDistinctForDateUsers, orcDistinctForDateUsers) =
-			measureDateQuery("2010-02-01T00:00:00.00Z", "2010-03-01T00:00:00.00Z", (t, start, end) => s"select count(distinct userId) from impressions_$t where date between '$start' and '$end'")
-
-		val (avroImpressions, parquetImpressions, orcImpressions) = measureQuery(t => s"select count(*) from impressions_$t")
-		val (avroDistinctUsers, parquetDistinctUsers, orcDistinctUsers) = measureQuery(t => s"select count(distinct userId) from impressions_$t")
-
-
 		logInfo(
 			"\n" +
 				Tabulator.format(
 					Seq(
 						Seq("Query", "Avro", "Parquet", "ORC"),
-						Seq("select count(*)", avroImpressions, parquetImpressions, orcImpressions),
-						Seq("select count(distinct userId)", avroDistinctUsers, parquetDistinctUsers, orcDistinctUsers),
-						Seq("select count(distinct userId) from impressions_avro where date between", avroDistinctForDateUsers, parquetDistinctForDateUsers, orcDistinctForDateUsers)
+						measureQuery(t => s"select count(*) from impressions_$t"),
+						measureQuery(t => s"select count(distinct userId) from impressions_$t"),
+						measureDateQuery("2010-02-01T00:00:00.00Z", "2010-03-01T00:00:00.00Z", (t, start, end) => s"select count(distinct userId) from impressions_$t where date between '$start' and '$end'")
 					)
 				)
 		)
