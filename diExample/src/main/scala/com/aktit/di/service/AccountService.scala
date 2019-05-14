e74bca5caa0d408a5f3519bd43adf70a41629caa
kostas.kougios@gmail.com <kostas.kougios@googlemail.com>
2019-05-14 08:34:44 +0100

    di example with guice

--- diExample/src/main/scala/com/aktit/di/service/AccountService.scala
+++ diExample/src/main/scala/com/aktit/di/service/AccountService.scala
@@ -17,6 +17,11 @@ class AccountService @Inject()(session: SparkSession, accountDao: AccountDao, tr
 
 	import session.implicits._
 
+	/**
+	  * Will execute all transfers to the accounts and change the last updated time of those accounts.
+	  *
+	  * @param time the time to set for the changed accounts
+	  */
 	def executeTransfers(time: Timestamp): Unit = {
 		val accounts = accountDao.read
 		val transfers = transferDao.read
@@ -24,12 +29,21 @@ class AccountService @Inject()(session: SparkSession, accountDao: AccountDao, tr
 		accountDao.append(resultedAccounts)
 	}
 
-	def executeTransfers(time: Timestamp, transfers: Dataset[Transfer], accounts: Dataset[Account]): Dataset[Account] =
+	/**
+	  * Splitting down executeTransfers to two methods helps with testing. Here we only have to pass datasets which are
+	  * easy to create.
+	  */
+	private[service] def executeTransfers(time: Timestamp, transfers: Dataset[Transfer], accounts: Dataset[Account]): Dataset[Account] =
 		accounts.as("l").joinWith(transfers.as("r"), $"l.name" === $"r.accountName")
 			.groupByKey {
 				case (account, transfer) => account.name
 			}.mapGroups {
 			(accountName, transfersPerAccount) =>
+				// Note: this runs in an executor. We can't call methods from AccountService here.
+				// This is because say a call to myMethod() is actually a call to
+				// this.myMethod() and spark would have to serialize AccountService in order
+				// for it to be able to access it like that.
+				// DI runs on the driver and the beans are not available on executors.
 				val a = transfersPerAccount.toArray
 				val account = a.head._1
 				val transfers = a.map(_._2)
