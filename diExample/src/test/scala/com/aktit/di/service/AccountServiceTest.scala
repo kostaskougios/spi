5522ae9ef5fb26e921fbbf28e277a88bb5b571e5
kostas.kougios@gmail.com <kostas.kougios@googlemail.com>
2019-05-14 08:24:26 +0100

    di example with guice

---
+++ diExample/src/test/scala/com/aktit/di/service/AccountServiceTest.scala
@@ -0,0 +1,42 @@
+package com.aktit.di.service
+
+import com.aktit.di.AbstractDiSuite
+import com.aktit.di.DiBuilders.{account, timestamp, transfer}
+
+/**
+  * @author kostas.kougios
+  *         14/05/19 - 08:17
+  */
+class AccountServiceTest extends AbstractDiSuite
+{
+	val transferTime = timestamp(2011, 10, 1, 1, 5)
+
+	import session.implicits._
+
+	test("transfers are executed by account name") {
+		new App
+		{
+			service.executeTransfers(
+				transferTime,
+				Seq(
+					transfer(accountName = "acc1", changeAmount = 5),
+					transfer(accountName = "acc2", changeAmount = 10)
+				).toDS,
+				Seq(
+					account(name = "acc1", amount = 1, lastUpdated = timestamp(2010, 5, 10, 8, 0)),
+					account(name = "acc2", amount = 2, lastUpdated = timestamp(2010, 8, 11, 8, 0))
+				).toDS
+			).toSet should be(Set(
+				account(name = "acc1", amount = 6, lastUpdated = transferTime),
+				account(name = "acc2", amount = 12, lastUpdated = transferTime)
+			))
+		}
+	}
+
+	class App
+	{
+		val app = createDiApp
+		val service = app.instance[AccountService]
+	}
+
+}
